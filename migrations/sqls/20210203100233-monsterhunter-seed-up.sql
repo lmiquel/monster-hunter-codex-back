095588f77d68d28accfaf2a69a855c92e0e751d8
Léa Miquel <leamiquel21@gmail.com>
2021-02-03 23:28:46 +0100

    small fix on DB

--- migrations/sqls/20210203100233-monsterhunter-seed-up.sql
+++ migrations/sqls/20210203100233-monsterhunter-seed-up.sql
@@ -8,31 +8,31 @@ INSERT INTO locations (name) VALUES
 
 INSERT INTO monster (name, picture, severable_tail) VALUES
 ('Odogaron', 'https://www.eclypsia.com/content/MonsterHunter/World/Monstres/14.png', 1),
+('Paolumu', 'https://www.eclypsia.com/content/MonsterHunter/World/Monstres/10.png', 0),
 ('Nergigante', 'https://www.eclypsia.com/content/MonsterHunter/World/Monstres/26.png', 1),
 ('Rathalos', 'https://www.eclypsia.com/content/MonsterHunter/World/Monstres/15.png', 1),
 ('Barioth', 'https://www.eclypsia.com/content/MonsterHunter/Iceborne/Monstres/Portraits/Barioth.png', 1),
 ('Tigrex', 'https://www.eclypsia.com/content/MonsterHunter/Iceborne/Monstres/Portraits/Tigrex.png', 1),
-('Paolumu', 'https://www.eclypsia.com/content/MonsterHunter/World/Monstres/10.png', 0),
 ('Teostra', 'https://www.eclypsia.com/content/MonsterHunter/World/Monstres/27.png', 1),
 ('Pukei pukei', 'https://www.eclypsia.com/content/MonsterHunter/World/Monstres/03.png', 1);
 
 INSERT INTO elements (fire, water, thunder, ice, dragon, monster_id) VALUES 
 ('⭐', '⭐', '⭐⭐', '⭐⭐⭐', '❌', 1),
-('⭐', '⭐', '⭐⭐⭐', '⭐', '⭐⭐', 2),
-('❌', '⭐', '⭐', '⭐⭐', '⭐⭐⭐', 3),
-('⭐⭐⭐', '❌', '⭐⭐', '❌', '⭐', 4),
-('❌', '⭐', '⭐⭐⭐', '⭐', '⭐⭐⭐', 5),
-('⭐⭐⭐', '❌', '⭐⭐', '⭐', '⭐', 6),
+('⭐⭐⭐', '❌', '⭐⭐', '⭐', '⭐', 2),
+('⭐', '⭐', '⭐⭐⭐', '⭐', '⭐⭐', 3),
+('❌', '⭐', '⭐', '⭐⭐', '⭐⭐⭐', 4),
+('⭐⭐⭐', '❌', '⭐⭐', '❌', '⭐', 5),
+('❌', '⭐', '⭐⭐⭐', '⭐', '⭐⭐⭐', 6),
 ('❌', '⭐⭐⭐', '⭐', '⭐⭐⭐', '⭐', 7),
 ('⭐⭐', '❌', '⭐⭐⭐', '⭐⭐', '⭐', 8);
 
 INSERT INTO ailments (poison, sleep, paralysis, blast, stun, monster_id) VALUES 
 ('⭐', '⭐⭐', '⭐⭐⭐', '⭐⭐', '⭐⭐', 1),
-('⭐⭐', '⭐⭐', '⭐⭐', '⭐⭐', '⭐⭐', 2),
-('⭐', '⭐⭐', '⭐⭐', '⭐', '⭐⭐', 3),
-('⭐⭐', '⭐⭐', '⭐⭐', '⭐⭐', '⭐⭐', 4),
+('⭐⭐', '⭐⭐', '⭐⭐', '⭐⭐⭐', '⭐⭐⭐', 2),
+('⭐⭐', '⭐⭐', '⭐⭐', '⭐⭐', '⭐⭐', 3),
+('⭐', '⭐⭐', '⭐⭐', '⭐', '⭐⭐', 4),
 ('⭐⭐', '⭐⭐', '⭐⭐', '⭐⭐', '⭐⭐', 5),
-('⭐⭐', '⭐⭐', '⭐⭐', '⭐⭐⭐', '⭐⭐⭐', 6),
+('⭐⭐', '⭐⭐', '⭐⭐', '⭐⭐', '⭐⭐', 6),
 ('⭐⭐', '⭐', '⭐', '⭐', '⭐⭐', 7),
 ('⭐', '⭐⭐⭐', '⭐⭐⭐', '⭐⭐', '⭐⭐', 8);
 
@@ -40,13 +40,13 @@ INSERT INTO ailments (poison, sleep, paralysis, blast, stun, monster_id) VALUES
 INSERT INTO monster_has_locations (monster_id, locations_id) VALUES
 (1, 4),
 (1, 3),
-(2, 5),
-(2, 2),
-(3, 1),
-(4, 6),
+(2, 3),
+(3, 5),
+(3, 2),
+(4, 1),
 (5, 6),
-(5, 4),
-(6, 3),
+(6, 6),
+(6, 4),
 (7, 2),
 (7, 5),
 (8, 1),
